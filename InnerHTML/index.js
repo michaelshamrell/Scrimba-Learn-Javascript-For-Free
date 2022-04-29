88417cd647bb4ecda899652e39f4bfce21c0098e
michaelshamrell <michaelshamrell2001@gmail.com>
2022-04-28 22:52:35 -0700

    Create a website that has a buy button using .innerHTML that includes an onclick fuction that thanks the user for their purchase.

--- InnerHTML/index.js
+++ InnerHTML/index.js
@@ -1,5 +1,9 @@
 const container = document.getElementById("container")
 
-container.innerHTML = "<button>Buy!</button>"
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
 
 // you can use .innerHTML to pass in html syntax and objects like buttons into the html directly
+
+function buy() {
+  container.innerHTML += "<p> Thank you for your purchase </p>"
+}

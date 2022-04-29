ce382e98166d8dbf10e153169482d5707538f026
michaelshamrell <michaelshamrell2001@gmail.com>
2022-04-29 15:51:13 -0700

    Find code on stack about how to grab the current tab url from chrome.

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -21,13 +21,25 @@ button.addEventListener("click", function() {
   localStorage.setItem("myLeads", JSON.stringify(myLeads) )
   render(myLeads)
 })
+
 deleteBtn.addEventListener("dblclick", function() {
   localStorage.clear()
   myLeads = []
   render(myLeads)
 })
+
 tabBtn.addEventListener("click", function() {
-console.log(tabs[0].url)
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    let activeTab = tabs[0];
+    let activeTabId = activeTab.id // or do whatever you need
+
+ })
+  myLeads.push(tabs[0].url)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+  render(myLeads)
 })
 
 function render(leads) {

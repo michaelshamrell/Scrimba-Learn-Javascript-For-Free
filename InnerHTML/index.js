7c3aa9926c1be6ff6fe00447fee037304646312b
michaelshamrell <michaelshamrell2001@gmail.com>
2022-04-28 22:48:17 -0700

    Take notes and exampes on .innerHTML in javascript.

---
+++ InnerHTML/index.js
@@ -0,0 +1,5 @@
+const container = document.getElementById("container")
+
+container.innerHTML = "<button>Buy!</button>"
+
+// you can use .innerHTML to pass in html syntax and objects like buttons into the html directly

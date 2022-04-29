34a3c1d41f9a8da415cf93a8875a7df7cf20b677
michaelshamrell <michaelshamrell2001@gmail.com>
2022-04-28 23:18:39 -0700

    Format the listItems string into a 'tempalte string'. These strings allow you to format the javacsript in a way that looks like html. This is definitely easier for people to read and understand.

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -15,7 +15,14 @@ button.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-      listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+      // listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+      listItems += 
+      `<li>
+          <a target='_blank' href='${myLeads[i]}'>
+              ${myLeads[i]}
+          </a>
+       </li>`
+
     }
       ulEl.innerHTML = listItems
 }

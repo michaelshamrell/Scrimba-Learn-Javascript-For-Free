b7a397ba25d4d909c5e89b4063084dc681e03367
michaelshamrell <michaelshamrell2001@gmail.com>
2022-04-29 20:07:36 -0700

    Create a start to a readme

--- Chrome Extension/README.md
+++ Chrome Extension/README.md
@@ -1,1 +1,12 @@
-You can use the variable localStorage 
+we used event listeners to control html in the js file
+we learned about .innerhtml 
+we learned input value to grab the value from the input field that the user puts in
+we learned about parameters, specifically the leads inside  of the render. Parameters go inside of the arenthesis in functions
+we learned template strings, `` which allow us to write code on multiple lines, we use ${} to reference js objects and functions
+
+we saved things in localStorage to make our own simple database
+we learned about json which allows us to store the data in js, we used JSON.stringify and JSON.parse to turn it into a javascript array
+We manipulated the manifest.JSON file to add permissions to allow the extension to be used in google
+
+we learned about placements in arrays and how to call on items in arrays object[0], etc...
+
--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,67 +1,47 @@
 let myLeads = []
-const button = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-//when the if statement is formatted like this it means that it is checking if it is truthy
+const tabBtn = document.getElementById("tab-btn")
+
 if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage
-  render(myLeads)
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-const tabs = [
-  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
-button.addEventListener("click", function() {
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-  render(myLeads)
-})
-
-deleteBtn.addEventListener("dblclick", function() {
-  localStorage.clear()
-  myLeads = []
-  render(myLeads)
-})
-
-tabBtn.addEventListener("click", function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
-    // since only one tab should be active and in the current window at once
-    // the return variable should only have one entry
-    let activeTab = tabs[0];
-    let activeTabId = activeTab.id // or do whatever you need
-
- })
-  myLeads.push(tabs[0].url)
-  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-  render(myLeads)
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
-      listItems += 
-      `<li>
-          <a target='_blank' href='${leads[i]}'>
-              ${leads[i]}
-          </a>
-       </li>`
-
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
     }
-      ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems
 }
-  //breaking this line of code into three streps, first ou are creating the li element, then creating the text content of the element, then taking the expression and apppending it to the unordered list.
-  // create Element
-  // set text Element
-  // append to ul
 
-  //Another way to do what's above--------------
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
-  // const li = document.createElement("li")
-  // li.textContent = myLeads[i]
-  // ulEl.append(li)
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
